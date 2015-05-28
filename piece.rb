a4a31c0c299fc15544f54482c2cd0cce665d98a2
Denny Tek <tek.denny@gmail.com>
2015-05-28 16:09:43 -0400

    Add piece.rb with individual class/initialize methods

---
+++ piece.rb
@@ -0,0 +1,47 @@
+class King
+
+	def initialize(color)
+		@color = color
+	end
+
+end
+
+class Queen
+
+	def initialize(color)
+		@color = color
+	end
+
+end
+
+class Rook
+
+	def initialize(color)
+		@color = color
+	end
+
+end
+
+class Bishop
+	
+	def initialize(color)
+		@color = color
+	end	
+
+end
+
+class Knight
+
+	def initialize(color)
+		@color = color
+	end
+
+end
+
+class Pawn
+
+	def initialize(color)
+		@color = color
+	end
+
+end
