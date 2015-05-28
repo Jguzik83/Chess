f4ec71c0406882d09ef49c992874023a6ae693bb
Jason Guzik <jguzik@gmail.com>
2015-05-28 19:14:40 -0400

    update controller, viewer, create gemfile for artii gem

--- controller.rb
+++ controller.rb
@@ -10,11 +10,35 @@ class Controller
 
   def run
     until game_finished?
-      Viewer.whos_turn(player)
-      Viewer.whats_move(player)
-      move = Viewer.get_move
+      Viewer.whos_turn(player) #white's turn
+      Viewer.whats_move(player) #white, your move?
+      position = Viewer.get_move #get the position, make downcase for ease
+
+      #check if that piece can get moved by the current player
+      #if invalid piece for that player or not a possible choice, make choose
+      #again (call run here?)
+
       #calculate moves method
-      Viewer.display_moves
+
+      Viewer.display_moves #moves for white pawn #{position}:
+      Viewer.where_to(player, position)#white, move #{position} where?
+
+      move_to = Viewer.get_move
+
+      #if valid_move? method
+        #if destination_has_enemy_piece?   #here is a capture
+          #move_piece method (return me the piece as string)
+          #Viewer.capture(piece)
+        #else  #here there is no captre
+          #move method
+          #Viewer.recap(player, piece, position, move_to) #need to pull out of hash the piece
+        #end
+      #else
+        #Viewer.invalid_move
+      #end
+
+
+      #
     end
   end
 
