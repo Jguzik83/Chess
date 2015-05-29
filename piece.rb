aaeebec9dd34865066ac5d1c10cac16df1191c2a
Denny Tek <tek.denny@gmail.com>
2015-05-29 15:47:49 -0400

    Add possible_moves variable for each piece

--- piece.rb
+++ piece.rb
@@ -11,8 +11,10 @@ end
 class King < Piece
 # The king moves exactly one vacant square in any direction:
 # forwards, backwards, left, right, or diagonally. It can also castle in conjunction with a rook.
+attr_reader :possible_moves
 	def initialize(color)
 		super(color)
+		@possible_moves = [[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1],[-1,0],[-1,1]]
 	end
 
 	def valid_moves?(move_arr) #array[dx,dy]
@@ -32,8 +34,18 @@ end
 class Queen < Piece
 # The queen moves any number of vacant squares in any direction:
 # forwards, backwards, left, right, or diagonally, in a straight line.
+attr_reader :possible_moves
 	def initialize(color)
 		super(color)
+		@possible_moves =
+		[
+		[[0,1],[0,2],[0,3],[0,4],[0,5],[0,6],[0,7],[0,8]],
+		[[0,-1],[0,-2],[0,-3],[0,-4],[0,-5],[0,-6],[0,-7],[0,-8]],
+		[[1,0],[2,0],[3,0],[4,0],[5,0],[6,0],[7,0],[8,0]],
+		[[-1,0],[-2,0],[-3,0],[-4,0],[-5,0],[-6,0],[-7,0],[-8,0]],
+		[[1,1],[2,2],[3,3],[4,4],[5,5],[6,6],[7,7],[8,8]],
+		[[-1,-1],[-2,-2],[-3,-3],[-4,-4],[-5,-5],[-6,-6],[-7,-7],[-8,-8]],
+		]
 	end
 
 	def valid_moves?(move_arr)
@@ -55,8 +67,16 @@ end
 class Rook < Piece
 	# The rook moves any number of vacant squares forwards, backwards, left, or right in a straight line.
 	# It also takes part, along with the king, in a special move called castling.
+	attr_reader :possible_moves
 	def initialize(color)
 		super(color)
+		@possible_moves =
+		[
+			[[0,1],[0,2],[0,3],[0,4],[0,5],[0,6],[0,7],[0,8]],
+			[[0,-1],[0,-2],[0,-3],[0,-4],[0,-5],[0,-6],[0,-7],[0,-8]],
+			[[1,0],[2,0],[3,0],[4,0],[5,0],[6,0],[7,0],[8,0]],
+			[[-1,0],[-2,0],[-3,0],[-4,0],[-5,0],[-6,0],[-7,0],[-8,0]]
+		]
 	end
 
 	def valid_moves?(move_arr)
@@ -77,9 +97,16 @@ end
 class Bishop < Piece
 	# The bishop moves any number of vacant squares diagonally in a straight line. Consequently, a bishop stays on squares of the same color throughout a game.
 	# The two bishops each player starts with move on squares of opposite colors.
-	attr_reader :move_pattern
+	attr_reader :possible_moves
 	def initialize(color)
 		super(color)
+		@possible_moves =
+		[
+			[[1,1],[2,2],[3,3],[4,4],[5,5],[6,6],[7,7],[8,8]],
+			[[-1,-1],[-2,-2],[-3,-3],[-4,-4],[-5,-5],[-6,-6],[-7,-7],[-8,-8]],
+
+		]
+
 	end
 
 	def valid_moves?(move_arr)
@@ -101,10 +128,11 @@ class Knight < Piece
 	# Consequently, the knight alternates its square color each time it moves.
 	# The knight is the only piece that jumps over any intervening piece(s) when moving
 	# (castling being the only special instance in which pieces jump over one another).
-	attr_reader :move_pattern
+	attr_reader :possible_moves
 	def initialize(color)
 		super(color)
 		@passable = true
+		@possible_moves = [[-1,2],[1,2],[-2,1],[2,1],[-2,-1],[2,-1],[-1,-2],[1,-2]]
 	end
 
 	def valid_moves?(move_arr)
@@ -128,9 +156,10 @@ class Pawn < Piece
 	# toward the opponent's side of the board. When there is an enemy piece one square diagonally ahead
 	# of the pawn, either left or right, then the pawn may capture that piece. A pawn can perform a special
 	# type of capture of an enemy pawn called en passant. If the pawn reaches a square on the back rank of the opponent, it promotes to the player's choice of a queen, rook, bishop, or knight
-	attr_reader :move_pattern
+	attr_reader :possible_moves
 	def initialize(color)
 		super(color)
+		@possible_moves = [[0,1]]
 	end
 
 	def valid_moves?(move_arr)
@@ -145,8 +174,3 @@ class Pawn < Piece
 		end
 	end
 end
-
-# p piece = King.new("white")
-# p pawn = Pawn.new("black")
-
-
