ee7ee966f42f69ddf1cbc1815778778c75e46be8
Denny Tek <tek.denny@gmail.com>
2015-05-29 07:27:11 -0400

    Set up inheritance to Piece, add passable attr, add piece specs

--- piece.rb
+++ piece.rb
@@ -1,43 +1,42 @@
 class Piece
 	#will be the parent class of King, Queen, Rook, Bishop, Knight, Pawn
-	attr_reader :color
+	attr_reader :color, :passable
 	def initialize(color)
 		@color = color
+		@passable = false
 	end
 end
 
-class King 
-# The king moves exactly one vacant square in any direction: 
+
+class King < Piece
+# The king moves exactly one vacant square in any direction:
 # forwards, backwards, left, right, or diagonally. It can also castle in conjunction with a rook.
 	attr_reader :max_move
 	def initialize(color)
-		@color = color
+		super(color)
 		@max_move = 1
 	end
 
 	def valid_moves?(move)
 		#Test method to move pieces
 		move >= @max_move
-
-
 	end
 
 	def to_s
 		if @color == 'white'
-			'♔'	
+			'♔'
 		else
 			'♚'
 		end
 	end
-
 end
 
-class Queen
-# The queen moves any number of vacant squares in any direction: 
+class Queen < Piece
+# The queen moves any number of vacant squares in any direction:
 # forwards, backwards, left, right, or diagonally, in a straight line.
-	attr_reader :color, :max_move
+	attr_reader :max_move
 	def initialize(color)
-		@color = color
+		super(color)
 		@max_move = 8
 	end
 
@@ -47,20 +46,20 @@ class Queen
 
 	def to_s
 		if @color == 'white'
-			'♕'	
+			'♕'
 		else
-			'♛'	
+			'♛'
 		end
 	end
 
 end
 
-class Rook
-	# The rook moves any number of vacant squares forwards, backwards, left, or right in a straight line. 
+class Rook < Piece
+	# The rook moves any number of vacant squares forwards, backwards, left, or right in a straight line.
 	# It also takes part, along with the king, in a special move called castling.
-	attr_reader :color, :max_move
+	attr_reader :max_move
 	def initialize(color)
-		@color = color
+		super(color)
 		@max_move = 8
 	end
 
@@ -78,14 +77,14 @@ class Rook
 
 end
 
-class Bishop
-	# The bishop moves any number of vacant squares diagonally in a straight line. Consequently, a bishop stays on squares of the same color throughout a game. 
+class Bishop < Piece
+	# The bishop moves any number of vacant squares diagonally in a straight line. Consequently, a bishop stays on squares of the same color throughout a game.
 	# The two bishops each player starts with move on squares of opposite colors.
-	attr_reader :color, :max_move
+	attr_reader :max_move
 	def initialize(color)
-		@color = color
+		super(color)
 		@max_move = 8
-	end	
+	end
 
 	def valid_moves?(move)
 		move >= @max_move
@@ -101,15 +100,16 @@ class Bishop
 
 end
 
-class Knight
-	# The knight moves on an extended diagonal from one corner of any 2×3 rectangle of squares to the furthest opposite corner. 
-	# Consequently, the knight alternates its square color each time it moves. 
-	# The knight is the only piece that jumps over any intervening piece(s) when moving 
+class Knight < Piece
+	# The knight moves on an extended diagonal from one corner of any 2×3 rectangle of squares to the furthest opposite corner.
+	# Consequently, the knight alternates its square color each time it moves.
+	# The knight is the only piece that jumps over any intervening piece(s) when moving
 	# (castling being the only special instance in which pieces jump over one another).
-	attr_reader :color, :max_move
+	attr_reader :max_move
 	def initialize(color)
-		@color = color
+		super(color)
 		@max_move = 8
+		@passable = true
 	end
 
 	def valid_moves?(move)
@@ -126,14 +126,14 @@ class Knight
 
 end
 
-class Pawn
-	# The pawn moves forward exactly one space, or optionally, two spaces when on its starting square, 
-	# toward the opponent's side of the board. When there is an enemy piece one square diagonally ahead 
-	# of the pawn, either left or right, then the pawn may capture that piece. A pawn can perform a special 
+class Pawn < Piece
+	# The pawn moves forward exactly one space, or optionally, two spaces when on its starting square,
+	# toward the opponent's side of the board. When there is an enemy piece one square diagonally ahead
+	# of the pawn, either left or right, then the pawn may capture that piece. A pawn can perform a special
 	# type of capture of an enemy pawn called en passant. If the pawn reaches a square on the back rank of the opponent, it promotes to the player's choice of a queen, rook, bishop, or knight
-	attr_reader :color, :max_move
+	attr_reader :max_move
 	def initialize(color)
-		@color = color
+		super(color)
 		@max_move = 1
 	end
 
--- piece_spec.rb
+++ piece_spec.rb
@@ -11,3 +11,80 @@
 # black chess bishop	♝	U+265D	&#9821;
 # black chess knight	♞	U+265E	&#9822;
 # black chess pawn	♟	U+265F	&#9823;
+
+require_relative 'piece'
+
+describe 'Piece' do
+
+  it 'should initialize a a King' do
+    piece = King.new('white')
+    expect(piece).to be_a King
+  end
+
+  it 'should have a color attribute' do
+    piece = King.new('white')
+    expect(piece.color).to eq('white')
+  end
+
+  it 'should initialize a a Queen' do
+    piece = Queen.new('black')
+    expect(piece).to be_a Queen
+  end
+
+    it 'should have a color attribute' do
+    piece = Queen.new('black')
+    expect(piece.color).to eq('black')
+  end
+
+  it 'should initialize a a Rook' do
+    piece = Rook.new('white')
+    expect(piece).to be_a Rook
+  end
+
+  it 'should have a color attribute' do
+    piece = Rook.new('black')
+    expect(piece.color).to eq('black')
+  end
+
+  it 'should initialize a a Bishop' do
+    piece = Bishop.new('white')
+    expect(piece).to be_a Bishop
+  end
+
+  it 'should have a color attribute' do
+    piece = Bishop.new('black')
+    expect(piece.color).to eq('black')
+  end
+
+  it 'should initialize a a Knight' do
+    piece = Knight.new('white')
+    expect(piece).to be_a Knight
+  end
+
+   it 'should have a color attribute' do
+    piece = Knight.new('white')
+    expect(piece.color).to eq('white')
+  end
+
+  it 'Knight should be passable' do
+    piece = Knight.new('black')
+    expect(piece.passable).to eq(true)
+  end
+
+  it 'should initialize a a Pawn' do
+    piece = Pawn.new('white')
+    expect(piece).to be_a Pawn
+  end
+
+  it 'should have a color attribute' do
+    piece = Pawn.new('black')
+    expect(piece.color).to eq('black')
+  end
+
+    it 'should not be passable' do
+    piece = Pawn.new('black')
+    expect(piece.passable).to eq(false)
+  end
+
+
+end
