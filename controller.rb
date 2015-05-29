a2bd175477744c2a487943f61f44233196a84e26
Jason <jguzik@gmail.com>
2015-05-29 15:48:06 -0400

    add switching players with consideration for invalid errors, change viewer with better wording

--- controller.rb
+++ controller.rb
@@ -2,57 +2,59 @@ require_relative 'viewer.rb'
 require_relative 'board.rb'
 
 class Controller
-  attr_reader :board
+  attr_reader :board, :players
 
   def initialize(board)
     @board = board
     system 'clear'
     Viewer.welcome_message
+    @players = ['white', 'black']
+    
     run
     
   end
 
   def run
     until game_finished?
-      players = ['white', 'black']
-      players.each do |player|
-        system 'clear'
-        puts board
-        Viewer.whos_turn(player) #white's turn
-        Viewer.whats_move(player) #white, your move?
-        position = Viewer.get_move #get the position, make downcase for ease
-        if board.valid_piece?(position, player) #true/false if correct color piece is selected
+      player = players.first
+      system 'clear'
+      puts board
+      Viewer.whos_turn(player) #white's turn
+      Viewer.whats_move(player) #white, your move?
+      position = Viewer.get_move #get the position, make downcase for ease
+      if board.valid_piece?(position, player) #true/false if correct color piece is selected
           
-          #method to get possible move locations
+        #method to get possible move locations
           
-          string = array_spliter#(array)
-          Viewer.display_moves(string)
-          Viewer.where_to(player, position)
-          move_to = Viewer.get_move
+        string = array_spliter#(array)
+        Viewer.display_moves(string)
+        Viewer.where_to(player, position)
+        move_to = Viewer.get_move
 
-          #if valid_move?(move_to, array) method
+         #if valid_move?(move_to, array) method
             
-            #if destination_has_enemy_piece?   #here is a capture
-              #move_piece method (return me the piece as string)
-              #Viewer.capture(piece)
-            #else  #here there is no captre
-              #move method
-              #Viewer.recap(player, piece, position, move_to) #need to pull out of hash the piece
-            #end
-          #else
-            #Viewer.invalid_move
-            #run(player) Think about this as well, because I need to return with correct player, not 
-            #go to the next player
-          #end
-        else
-          Viewer.invalid_piece
+           #if destination_has_enemy_piece?   #here is a capture
+             #move_piece method (return me the piece as string)
+             #Viewer.capture(piece)
+           #else  #here there is no captre
+             #move method
+             #Viewer.recap(player, piece, position, move_to) #need to pull out of hash the piece
+           #end
+         #else
+           #Viewer.invalid_move
+           #run(player) Think about this as well, because I need to return with correct player, not 
+           #go to the next player
+         #end
+       else
+         Viewer.invalid_piece
           sleep(0.5)
-          run(player) #think about this, because we need to return with
+          run #think about this, because we need to return with
               #correct player whos turn it is
-        end
+       end
 
-      end
-    end
+     players.reverse!
+     end
+    
   end
 
   def valid_selection?(position)
